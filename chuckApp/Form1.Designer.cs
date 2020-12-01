0ab35fb9a9dfbfd45b95a1bbe4ceec6c910bde8c
Valeria Avila <valeria.avila17@gmail.com>
2020-12-01 06:08:07 -0800

    Added button to get joke and comboBox that shows categories

---
+++ chuckApp/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace chuckApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.getJokeBtn = new System.Windows.Forms.Button();
+            this.CategoryBox = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // getJokeBtn
+            // 
+            this.getJokeBtn.Location = new System.Drawing.Point(110, 75);
+            this.getJokeBtn.Name = "getJokeBtn";
+            this.getJokeBtn.Size = new System.Drawing.Size(245, 91);
+            this.getJokeBtn.TabIndex = 0;
+            this.getJokeBtn.Text = "Get Joke!";
+            this.getJokeBtn.UseVisualStyleBackColor = true;
+            this.getJokeBtn.Click += new System.EventHandler(this.getJokeBtn_Click);
+            // 
+            // CategoryBox
+            // 
+            this.CategoryBox.FormattingEnabled = true;
+            this.CategoryBox.Location = new System.Drawing.Point(110, 201);
+            this.CategoryBox.Name = "CategoryBox";
+            this.CategoryBox.Size = new System.Drawing.Size(245, 28);
+            this.CategoryBox.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.CategoryBox);
+            this.Controls.Add(this.getJokeBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button getJokeBtn;
+        private System.Windows.Forms.ComboBox CategoryBox;
+    }
+}
+
---
+++ chuckApp/Form1.cs
@@ -0,0 +1,38 @@
+﻿using ChuckNorrisAPI;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace chuckApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            getCategories();
+        }
+
+        private async void getJokeBtn_Click(object sender, EventArgs e)
+        {
+            Joke joke = await ChuckNorrisClient.GetRandomJoke();
+
+            MessageBox.Show(joke.JokeText);
+        }
+
+        private async void getCategories()
+        {
+            IEnumerable<String> categories = await ChuckNorrisClient.GetCategories();
+            foreach (var category in categories)
+            {
+                CategoryBox.Items.Add(category);
+            }
+        }
+    }
+}
